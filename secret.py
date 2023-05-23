f89b407203e70a8f0a176b04bd8239b5763247a5
Stephen Chesnowitz <schesnowitz@users.noreply.github.com>
2023-05-22 21:45:00 -0400

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,17 +1,1 @@
-<<<<<<< HEAD
-OPENAI_API_KEY='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' 
-SERPAPI_API_KEY='693bd7e4e40edc19531d2feb333056fa04f1eaf9405de743931b2cc5ec54ed10'
-HUGGINGFACEHUB_API_TOKEN='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'
-GOOGLE_MAPS="AIzaSyAk2mHJkuV9KEVGZIwijkPyMMYFa4QJdXU" 
-WEAVIATE_API_KEY="Jb2rq0pMAX3taJca0wmJxKVg1l6YRVIoX7nm"
-WEAVIATE_URL="https://demo-kq5kh12v.weaviate.network"
-APIFY_API_TOKEN="apify_api_pufphlrThjXoNEYABMOeZduKXJaQcV0EJKwD"
-REPLICATE_API_TOKEN="r8_LLNPx27oGjxqjPSFyOmL5wj6HcZ1Hv01ytS5b"
-PGDATABASE='railway'
-PGHOST='containers-us-west-32.railway.app'
-PGPASSWORD='wgZLXggkz8pRHgNeYFnk'
-PGPORT='6881'
-PGUSER='postgres'
-=======
 
->>>>>>> c9e9a44cb4ec2ebf91a54f7ce1045d5c11c99fa4
